2c70903137216b632c1e2ed744fdd6422eb1c30b
Zain Khan Swati <zainkhanswati54@gmail.com>
2026-06-17 12:53:54 +0500

    Delete gta-clone-3d directory

--- gta-clone-3d/src/weapons/Pistol.js
+++
@@ -1,8 +0,0 @@
-import { Weapon } from './Weapon.js';
-import { SETTINGS } from '../config/settings.js';
-
-export class Pistol extends Weapon {
-  constructor() {
-    super('Pistol', SETTINGS.weapons.pistolFireDelay, SETTINGS.weapons.pistolDamage);
-  }
-}
